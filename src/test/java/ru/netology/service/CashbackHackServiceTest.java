150a31b82c9084881ae2655416581744e1632f7c
Vladislav Romanov <conter123@list.ru>
2024-02-10 11:32:04 +0300

    Added tests with TestNG

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,31 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+    @Test
+    void shouldCalculateCashbackHack() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1900;
+
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    void shouldNotCalculateCashbackHackIfAmountMultipleOf1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 2000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
+
+
+}
